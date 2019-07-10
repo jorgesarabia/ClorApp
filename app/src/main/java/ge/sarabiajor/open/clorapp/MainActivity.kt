2ce23ac2f1b4215b645457a50e052bcec61fcb2a
Jorge Sarabia <ingjorgesarabia@gmail.com>
2019-07-10 10:42:02 -0300

    won

--- app/src/main/java/ge/sarabiajor/open/clorapp/MainActivity.kt
+++ app/src/main/java/ge/sarabiajor/open/clorapp/MainActivity.kt
@@ -1,12 +1,54 @@
 package ge.sarabiajor.open.clorapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    //Constantes
+    private val RC_SIGN_IN = 1
+
+    //extension functions:
+    fun Context.toast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth.getCurrentUser() != null) {
+            toast("Ya está logueado")
+        } else {
+            //No está logueado, se tiene que pasar al activity:
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(arrayListOf(
+                        AuthUI.IdpConfig.GoogleBuilder().build(),
+                        AuthUI.IdpConfig.EmailBuilder().build()
+                    )).build(),
+                RC_SIGN_IN)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        toast("HOLA")
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                toast("Se logueó")
+            } else if (resultCode == RESULT_CANCELED) {
+                toast("No se logueó")
+                finish()
+            }
+        }
+    }
+
+
 }
