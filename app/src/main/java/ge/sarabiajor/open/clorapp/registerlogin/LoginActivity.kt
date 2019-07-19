b3666302d1d55dce8cb9e548b6e91a7729b7bc90
Jorge Sarabia <ingjorgesarabia@gmail.com>
2019-07-19 12:59:06 -0300

    Se abre para crear una cuenta

--- app/src/main/java/ge/sarabiajor/open/clorapp/registerlogin/LoginActivity.kt
+++ app/src/main/java/ge/sarabiajor/open/clorapp/registerlogin/LoginActivity.kt
@@ -1,15 +1,39 @@
 package ge.sarabiajor.open.clorapp.registerlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import ge.sarabiajor.open.clorapp.MainActivity
 import ge.sarabiajor.open.clorapp.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    companion object{
+        val TAG = "LoginActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         //Escondo la barra de titulo para el Login:
         supportActionBar?.hide()
+        button_login.setOnClickListener {
+            performLogin()
+        }
+
+        textview_crear_cuenta_login.setOnClickListener {
+            Log.d(TAG,"Ir al Register")
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun performLogin(){
+        val message = "Se va loguear usando:" + edittext_email_login.text.toString() +
+                " y " + edittext_password_login.text.toString()
+
+        Log.d(TAG,message)
     }
 }
