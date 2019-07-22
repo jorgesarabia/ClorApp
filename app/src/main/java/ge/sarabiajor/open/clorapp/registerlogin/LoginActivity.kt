0fda4ae9998726d9d1b0c396eab27cd5bd5917fb
Jorge Sarabia <ingjorgesarabia@gmail.com>
2019-07-22 19:20:18 -0300

    Se evita que el teclado aparezca al iniciar un activity

--- app/src/main/java/ge/sarabiajor/open/clorapp/registerlogin/LoginActivity.kt
+++ app/src/main/java/ge/sarabiajor/open/clorapp/registerlogin/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import ge.sarabiajor.open.clorapp.MainActivity
